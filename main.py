db6b189f084ee8d7da81f04259a9e9785085ee29
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2025-06-24 13:00:43 +0530

    reporting update

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-from utils.result_fetcher import fetch_test_results
-from utils.chart_generator import generate_pie_chart
-from utils.pdf_generator import generate_pdf_report
-from gui.main_window import launch_gui
-
-
-def main():
-    mode = input("Enter mode (gui/cli): ").strip().lower()
-
-    if mode == "gui":
-        launch_gui()
-    else:
-        print("[*] CLI Mode: Generating report...")
-        df = fetch_test_results()
-        chart_path = "reports/charts/execution_summary.png"
-        generate_pie_chart(df, output_dir="reports/charts")
-        generate_pdf_report(df, chart_path)
-
+from gui.main_gui import run_gui
 
 if __name__ == "__main__":
-    main()
+    run_gui()
